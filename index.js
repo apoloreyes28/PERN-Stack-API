f26f28fd0370ad94d98d556756a94685100ac251
apoloreyes28 <reyesapolinar31@gmail.com>
2024-02-28 07:12:04 -0700

    REST API GET Method setup & testing

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
 });
+
+// GET   /books     --> obtener todos los libros
+app.get("/books", async (req, res) => {
+    try {
+        res.status(200).json({ message: "books are returned" });
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+
+// GET   /books/:id  --> obtener un libro en especifíco
+app.get("/books/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        res.status(200).json({ message: `specific book is returned with id: ${id}`});
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+
