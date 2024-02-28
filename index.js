c6404bba06ab15310ae2867b1a17485096f11c3d
apoloreyes28 <reyesapolinar31@gmail.com>
2024-02-28 07:24:49 -0700

    REST API POST method setup & testing

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
 });
+
 
 // GET   /books     --> obtener todos los libros
 app.get("/books", async (req, res) => {
@@ -22,6 +27,17 @@ app.get("/books/:id", async (req, res) => {
     try {
         const {id} = req.params;
         res.status(200).json({ message: `specific book is returned with id: ${id}`});
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+
+// POST   /books   --> crear un libro
+app.post("/books", async (req, res) => {
+    try {
+        const {name, description} = req.body;
+        res.status(201).json({ message: `book was created: ${name}, ${description}` });
     } catch (error) {
         res.json({ error: error.message });
     }
